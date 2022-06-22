39af0d867ac502a574f23273aa6a52ff2c2296ca
Konradhbr <32590845+Konradhbr@users.noreply.github.com>
2022-06-22 18:28:04 +0200

    Create landing page to test

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,29 @@
 function getValues() {
-	var userName = document.getElementById("name").value;
-	var userCompany = document.getElementById("company").value;
-	var userPhoneNumber = document.getElementById("mobile-number").value;
-	var userEmail = document.getElementById("email").value;
-	var userMessage = document.getElementById("message").value;	
+  var userName = document.getElementById('name').value;
+  var userCompany = document.getElementById('company').value;
+  var userPhoneNumber = document.getElementById('mobile-number').value;
+  var userEmail = document.getElementById('email').value;
+  var userMessage = document.getElementById('message').value;
 }
 
 function disappear() {
-	var email = document.getElementById('email').value;
-	var placeholder = document.getElementById('holderReq');
-	if (email != " ") {
-		placeholder.style.display = 'none';
-	}
-	else {}
-};
+  var email = document.getElementById('email').value;
+  var placeholder = document.getElementById('holderReq');
+  if (email != ' ') {
+    placeholder.style.display = 'none';
+  } else {
+  }
+}
 
 function sendForm() {
-	// axios.post(`https://tecra.space/api/spacewalkers/training`, {email: userEmail}).then(res => {
-	// 	if (res.status === 200) {
-			window.location.reload();
+  // axios.post(`https://tecra.space/api/spacewalkers/training`, {email: userEmail}).then(res => {
+  // 	if (res.status === 200) {
+  window.location.reload();
 
-	// 	}
-	// });
+  // 	}
+  // });
 }
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
